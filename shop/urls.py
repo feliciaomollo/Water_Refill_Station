a946e22ab44d8ef1d3cf99a4eddc54d434ac5caa
Feliciaomollo <48946836+andyomollo@users.noreply.github.com>
2026-06-30 13:40:00 +0300

    Add customer list view, template, and URL~

--- shop/urls.py
+++ shop/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('test/', views.test_base, name='test_base'),
+    path('customers/', views.customer_list, name='customer_list'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Customer
 
 def test_base(request):
     return render(request, 'shop/base.html')
+
+def customer_list(request):
+    customers = Customer.objects.all()     
+    return render(request, 'shop/customer_list.html', {'customers': customers})
