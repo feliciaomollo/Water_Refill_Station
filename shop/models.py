bf103c338a0d981e84406d4fbb6218910bcb7d6d
Feliciaomollo <48946836+andyomollo@users.noreply.github.com>
2026-06-30 12:04:48 +0300

    Add Customer model with admin registration

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=15)
+    location = models.CharField(max_length=255)
+    is_credit_customer = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
